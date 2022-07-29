62fb4be0a40d73e02a24eca474d6bee3abb8d9c0
unknown <kulyanasun@gmail.com>
2022-07-29 17:42:58 +0200

    first Push

--- ecommerce-user-service/src/test/java/com/dailycodebuffer/springboot/tutorial/controller/UserControllerTest.java
+++ ecommerce-user-service/src/test/java/com/dailycodebuffer/springboot/tutorial/controller/UserControllerTest.java
@@ -4,7 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
