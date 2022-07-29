057a3282024a6cbe89b7539a594b3c3efbba5d29
unknown <kulyanasun@gmail.com>
2022-07-29 13:10:41 +0200

    first Push

--- ecommerce-user-service/src/main/java/com/dailycodebuffer/springboot/tutorial/controller/UserController.java
+++ ecommerce-user-service/src/main/java/com/dailycodebuffer/springboot/tutorial/controller/UserController.java
@@ -1,7 +1,16 @@
 package com.dailycodebuffer.springboot.tutorial.controller;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,23 +22,80 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@RequestMapping(value = {"/api/users"})
+@RequestMapping(value = { "/api/users" })
 @Slf4j
 @RequiredArgsConstructor
 public class UserController {
 
 	private final UserService userService;
-	
+
 	@GetMapping
 	public ResponseEntity<DtoCollectionResponse<UserDto>> findAll() {
-	
+
 		log.info("*** UserDto List, controller; fetch all users *");
-	
+
 		return ResponseEntity.ok(new DtoCollectionResponse<>(this.userService.findAll()));
-			
+
+	}
+
+	@GetMapping("/{userId}")
+	public ResponseEntity<UserDto> findById(
+			@PathVariable("userId") @NotBlank(message = "Input must not blank") @Valid final String userId) {
+
+		log.info("*** UserDto, Controller; fetch user by id *");
+
+		return ResponseEntity.ok(this.userService.findById(Integer.parseInt(userId.strip())));
+
+	}
+	
+	@GetMapping("/username/{username}")
+	public ResponseEntity<UserDto> findByUsername(
+			@PathVariable("username") 
+			@NotBlank(message = "Input must not blank") 
+			@Valid final String username) {
+		return ResponseEntity.ok(this.userService.findByUsername(username));
+	}
+
+	
+	
+	@PostMapping
+	public ResponseEntity<UserDto> save(
+			@RequestBody 
+			@NotNull(message = "Input must not NULL") 
+			@Valid final UserDto userDto) {
+		log.info("*** UserDto, resource; save user *");
+		return ResponseEntity.ok(this.userService.save(userDto));
+	}
+	
+	
+	@PutMapping
+	public ResponseEntity<UserDto> update(
+			@RequestBody 
+			@NotNull(message = "Input must not NULL") 
+			@Valid final UserDto userDto) {
+		log.info("*** UserDto, resource; update user *");
+		return ResponseEntity.ok(this.userService.update(userDto));
+	}
+	
+	
+	@PutMapping("/{userId}")
+	public ResponseEntity<UserDto> update(
+			@PathVariable("userId") 
+			@NotBlank(message = "Input must not blank") final String userId, 
+			@RequestBody 
+			@NotNull(message = "Input must not NULL") 
+			@Valid final UserDto userDto) {
+		log.info("*** UserDto, resource; update user with userId *");
+		return ResponseEntity.ok(this.userService.update(Integer.parseInt(userId.strip()), userDto));
 	}
 	
 	
 	
+	@DeleteMapping("/{userId}")
+	public ResponseEntity<Boolean> deleteById(@PathVariable("userId") @NotBlank(message = "Input must not blank") @Valid final String userId) {
+		log.info("*** Boolean, resource; delete user by id *");
+		this.userService.deleteById(Integer.parseInt(userId));
+		return ResponseEntity.ok(true);
+	}
 	
 }
--- ecommerce-user-service/src/test/java/com/dailycodebuffer/springboot/tutorial/controller/UserControllerTest.java
+++ ecommerce-user-service/src/test/java/com/dailycodebuffer/springboot/tutorial/controller/UserControllerTest.java
@@ -2,6 +2,7 @@ package com.dailycodebuffer.springboot.tutorial.controller;
 
 import org.springframework.http.HttpStatus;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,21 +11,27 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.mockito.BDDMockito.given;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.springframework.http.MediaType;
 
+import java.util.Collection;
 import java.util.List;
-
-import com.dailycodebuffer.springboot.tutorial.domain.User;
+import java.util.Optional;
+import javax.ws.rs.core.Response;
 import com.dailycodebuffer.springboot.tutorial.dto.UserDto;
+import com.dailycodebuffer.springboot.tutorial.exception.wrapper.UserObjectNotFoundException;
 import com.dailycodebuffer.springboot.tutorial.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @AutoConfigureJsonTesters
 @WebMvcTest(UserController.class)
@@ -36,45 +43,216 @@ public class UserControllerTest {
 	@Autowired
 	private MockMvc mvc;
 
+	// This object will be magically initialized by the initFields method below.
+	private JacksonTester<UserDto> json;
 
 	// This object will be magically initialized by the initFields method below.
-    private JacksonTester<User> json;
+	private JacksonTester<Collection<UserDto>> jsonCollective;
 
-    private UserDto userDto;
-    
-     
+	private UserDto userDto;
 
-    @BeforeEach
-    public void setup()
-    {
-    	
-    	userDto = UserDto.builder().email("skulyana@gmail.com").firstName("Sunil").lastName("Kulyana").imageUrl("google.com").phone("111").build();
-    
-    }
-    
-    @Test
-    void shouldFetchAllUsers() throws Exception {
-    	
-    	given(userService.findAll()).willReturn(List.of(userDto));
-    	
-    	// when
-        MockHttpServletResponse response = mvc.perform(
-                get("/api/users")
-                        .accept(MediaType.APPLICATION_JSON))
-                .andReturn().getResponse();
-    	
+	@BeforeEach
+	public void setup() {
+		JacksonTester.initFields(this, new ObjectMapper());
+		userDto = UserDto.builder().email("skulyana@gmail.com").firstName("Sunil").lastName("Kulyana")
+				.imageUrl("google.com").phone("111").build();
 
-        // then
-        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
-    	
-    }
-    
-    
+	}
+
+	@Test
+	void shouldFetchAllUsers() throws Exception {
+
+		given(userService.findAll()).willReturn(List.of(userDto));
+
+		// when
+		MockHttpServletResponse response = mvc.perform(get("/api/users").accept(MediaType.APPLICATION_JSON)).andReturn()
+				.getResponse();
+
+		// then
+		assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+
+	}
+
+	@Test
+	void shouldFetchUserById() throws Exception {
+
+		given(userService.findById(1)).willReturn(Optional.of(userDto).get());
+
+		Integer userId = 1;
+
+		// when
+		MockHttpServletResponse response = mvc
+				.perform(get("/api/users/{userId}", userId).accept(MediaType.APPLICATION_JSON)).andReturn()
+				.getResponse();
+
+		// then
+		assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+
+		assertThat(response.getContentAsString()).isEqualTo(json.write(userDto).getJson());
+
+	}
+
+	@Test
+	void shouldReturn404WhenFetchUserById_Does_Not_Exist() throws Exception {
+
+		given(userService.findById(1)).willThrow(UserObjectNotFoundException.class);
+
+		Integer userId = 1;
+
+		// when
+		MockHttpServletResponse response = mvc
+				.perform(get("/api/users/{userId}", userId).accept(MediaType.APPLICATION_JSON)).andReturn()
+				.getResponse();
+
+		// then
+		assertThat(response.getStatus()).isEqualTo(Response.Status.NOT_FOUND.getStatusCode());
+
+	}
+	
+	@Test
+	void shouldFetchUserByName() throws Exception {
+
+		given(userService.findByUsername("skulyana")).willReturn(Optional.of(userDto).get());
+
+		Integer userId = 1;
+		
+		String username = "skulyana";
+
+		// when
+		MockHttpServletResponse response = mvc
+				.perform(get("/api/users/username/{username}", username).accept(MediaType.APPLICATION_JSON)).andReturn()
+				.getResponse();
+
+		// then
+		assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+
+		assertThat(response.getContentAsString()).isEqualTo(json.write(userDto).getJson());
+
+	}
+
+	
+	@Test
+	void shouldReturn404WhenFetchUserByName_Does_Not_Exist() throws Exception {
+
+		given(userService.findByUsername("skulyana")).willThrow(UserObjectNotFoundException.class);
+
+		String username = "skulyana";
+
+		// when
+		MockHttpServletResponse response = mvc
+				.perform(get("/api/users/username/{username}", username).accept(MediaType.APPLICATION_JSON)).andReturn()
+				.getResponse();
+
+		// then
+		assertThat(response.getStatus()).isEqualTo(Response.Status.NOT_FOUND.getStatusCode());
+
+	}
+
+	
+
+	// JUnit test for createUser method in Controller
+	@DisplayName("JUnit test for createUser method")
+	@Test
+	public void shouldCreateNewUser() throws Exception {
+
+		given(userService.save(userDto)).willReturn(userDto);
+
+		// when
+		MockHttpServletResponse response = mvc.perform(
+				post("/api/users").contentType(MediaType.APPLICATION_JSON).content(json.write(userDto).getJson()))
+				.andReturn().getResponse();
+		// then
+		assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+
+		assertThat(response.getContentAsString()).isEqualTo(json.write(userDto).getJson());
+
+	}
+
+	
+	// JUnit test for updateUser method in Controller
+	@DisplayName("JUnit test for updateUser method")
+	@Test
+	public void shouldUpdateUser_When_UserObject_Given() throws Exception {
+
+		given(userService.update(userDto)).willReturn(userDto);
+
+		// when
+		MockHttpServletResponse response = mvc.perform(
+				put("/api/users").contentType(MediaType.APPLICATION_JSON).content(json.write(userDto).getJson()))
+				.andReturn().getResponse();
+		// then
+		assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+
+		assertThat(response.getContentAsString()).isEqualTo(json.write(userDto).getJson());
+
+	}
+	
+	
+	// JUnit test for updateUser method in Controller
+		@DisplayName("JUnit test for updateUser method when userId is given")
+		@Test
+		public void shouldUpdateUser_When_UserId_Given() throws Exception {
+
+			Integer userId = 1;
+			
+			given(userService.update(userId,userDto)).willReturn(userDto);
+
+			// when
+			MockHttpServletResponse response = mvc.perform(
+					put("/api/users/{userId}", userId).contentType(MediaType.APPLICATION_JSON).content(json.write(userDto).getJson()))
+					.andReturn().getResponse();
+			// then
+			assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+
+			assertThat(response.getContentAsString()).isEqualTo(json.write(userDto).getJson());
+
+		}
+		
+
+		@Test
+		void shouldReturn404When_Update_User_By_Id_Does_Not_Exist() throws Exception {
+
+			Integer userId=1;
+			given(userService.update(userId,userDto)).willThrow(UserObjectNotFoundException.class);
+
+			String username = "skulyana";
+
+			// when
+			MockHttpServletResponse response = mvc.perform(
+					put("/api/users/{userId}", userId).contentType(MediaType.APPLICATION_JSON).content(json.write(userDto).getJson()))
+					.andReturn().getResponse();
+
+			// then
+			assertThat(response.getStatus()).isEqualTo(Response.Status.NOT_FOUND.getStatusCode());
+
+		}
+
+
 	
 	
 	
 	
+	// JUnit test for createUser method in Controller
+	@DisplayName("JUnit test for deleteUser method")
+	@Test
+	public void shouldDeleteNewUser() throws Exception {
+
+		Integer userId = 1;
+
+		// when
+		MockHttpServletResponse response = mvc
+				.perform(delete("/api/users/{userId}", userId).accept(MediaType.APPLICATION_JSON)).andReturn()
+				.getResponse();
+
+		// then
+		assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+
+		assertThat(new Boolean(response.getContentAsString())).isEqualTo(true);
+
+		// then - verify the output
+
+		verify(userService, times(1)).deleteById(userId);
+
+	}
+
 }
-
-
-
