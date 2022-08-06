1c0c6efdd817d11e6ce383a22e9cc9e1692af9b8
unknown <kulyanasun@gmail.com>
2022-08-06 16:05:58 +0200

    Initialise

---
+++ cloud-config-amazon/src/test/java/com/dailycodebuffer/springboot/tutorial/CloudConfigAmazonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailycodebuffer.springboot.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigAmazonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
